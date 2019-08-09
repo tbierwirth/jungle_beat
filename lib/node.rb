5d848aabd3cc183f3aafab1c85537950439737c2
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-09 16:06:44 -0600

    Refactor to remove data method and use an attr_reader

--- lib/node.rb
+++ lib/node.rb
@@ -1,12 +1,8 @@
 class Node
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def data
-    p name
+  attr_reader :data
+  
+  def initialize(data)
+    @data = data
   end
 
   def next_node
